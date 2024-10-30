3b835fd99920b303625f8d92b974a247eaa03b94
mhadi1091 <mhadimalik2004@gmail.com>
2024-10-30 13:37:23 +0500

    lab tasks 1

---
+++ Lab2.java
@@ -0,0 +1,24 @@
+// import java.util.Scanner;
+// public class Lab2 {
+//     public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter your height in meters:");
+//        float height = input.nextFloat();
+//        System.out.println("Enter your weight in KGs:");
+//        float weight = input.nextFloat();
+//        float BMI = weight/(height * height);
+//       if (BMI < 18.5) {
+//         System.out.println("BMI : underweight");
+//       }
+//       else if(BMI >= 18.5 && BMI <= 24.9){
+//         System.out.println("BMI : Normal");
+//       }
+//       else if(BMI >= 25 && BMI <= 29.9){
+//         System.out.println("BMI : Over Weight");
+//       }
+//       else{
+//         System.out.println("BMI : obese");
+//       }
+//     }
+// }
+
